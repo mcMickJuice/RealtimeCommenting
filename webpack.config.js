e6cc21f7abc54065c79d4b23a498c97b1f683f07
Mike Joyce <mikejoyce19@gmail.com>
2015-10-23 23:21:28 -0500

    Added less loader and less loader rule

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
 		loaders: [
 			{test: /\.css$/, loader: "style!css"},
 			{test: /\.js$/, exclude: /(node_module|bower_components)/,loader: "babel"},
-			{test: /\.jsx$/, exclude: /(node_modules|bower_components)/, loader: 'babel'}
+			{test: /\.jsx$/, exclude: /(node_modules|bower_components)/, loader: 'babel'},
+			{test: /\.less$/, exclude: /(node_modules|bower_components)/, loader: 'style!css!less'}
 		]
 	},
 	devtool: '#inline-source-map'
